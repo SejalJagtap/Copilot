3b0633c37e675ca2a76b7af375569f92f0c29153
Sejal Jagtap <sejaljagtap1309@gmail.com>
2024-02-05 16:25:01 +0530

    description1Added

--- app/components/chat/examples/index.tsx
+++ app/components/chat/examples/index.tsx
@@ -23,7 +23,7 @@ const Examples: FC<IExamplProps>=({
     // const inputRef = useRef(null);
     return(
       <div className='mx-auto h-full pc:w-[794px] max-w-full mobile:w-full px-3.5 '>
-        <div className="absolute border-gray-200sm:mx-0 mx-5 mt-20 max-w-screen-md rounded-md border sm:w-full">
+        <div className=" border-gray-200sm:mx-0 mx-5 mt-20 max-w-screen-md rounded-md border sm:w-full">
           <div className="flex flex-col space-y-4 p-7 sm:p-10">
             <h1 className="text-lg font-semibold text-black">
               Welcome to ChatHN!
